4f809cdc0b676b54786c598209f00845977331e4
to7ma3na3 <83623295+to7ma3na3@users.noreply.github.com>
2022-04-09 20:30:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+  const bible1 = document.querySelector('.bible1');
+  const sentence1 = document.querySelector('.sentence1');
+  bible1.addEventListener('click', () => {
+    sentence1.classList.toggle('appear');
+  });
+
+  const bible2 = document.querySelector('.bible2');
+  const sentence2 = document.querySelector('.sentence2');
+  bible2.addEventListener('click', () => {
+    sentence2.classList.toggle('appear');
+  });
+
+  const bible3 = document.querySelector('.bible3');
+  const sentence3 = document.querySelector('.sentence3');
+  bible3.addEventListener('click', () => {
+    sentence3.classList.toggle('appear');
+  });
+
